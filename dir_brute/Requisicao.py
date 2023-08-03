ce138a72cffd01040677c1cd4d44b1d2c40471ef
Giovanny Matos <99231397+GiovanniMatos@users.noreply.github.com>
2023-08-03 11:39:10 -0300

    Add files via upload

---
+++ dir_brute/Requisicao.py
@@ -0,0 +1,43 @@
+import requests
+import urllib.request
+from os import path
+
+
+print(" ex: https://httpbin.org")
+print()
+url = input(" URL: ")
+print()
+response = requests.get(url)
+r = response.status_code
+if (r == 200):
+    print("\033[1;32m[+] Site Ativo \033[0;0m")
+
+response = requests.get(url, verify=True)
+r = response.status_code
+if (r == 200):
+    print("\033[1;32m[+] SSL válido \033[0;0m")
+
+
+print("="*15, "Diretorios","="*15)
+print()
+
+def main():
+    website = url
+    words_dir = 'diretorios.txt'
+
+    diretorios = path.realpath(words_dir)
+
+    with open(diretorios) as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+
+    for word in lines:
+        directory = website + word
+        try:
+            print(f'\033[1;32m[{urllib.request.urlopen(directory).getcode()}]\033[0;0m {word} ')
+        except Exception as error:
+            pass
+if __name__ == '__main__':
+    main()
+print("tente /.htaccess")
+print("tente /web.config")
